bfd9a8a669393de6da276b93b362851e66f16520
Patricccc <pattytroller@gmail.com>
2021-06-09 20:16:06 -0400

    Create script.js

---
+++ While Loops/Hacker Speak/script.js
@@ -0,0 +1,27 @@
+function hacker() {
+  var sentence = document.getElementById("input").value;
+  var i = 0;
+  var output = "";
+  while (true) {
+    if (i == sentence.length) {
+      break;
+    }
+    else if (sentence.charAt(i).toLowerCase() == "a") {
+      output = output + "4";
+    }
+    else if (sentence.charAt(i).toLowerCase() == "e") {
+      output = output + "3";
+    }
+    else if (sentence.charAt(i).toLowerCase() == "i") {
+      output = output + "1";
+    }
+    else if (sentence.charAt(i).toLowerCase() == "o") {
+      output = output + "0";
+    }
+    else {
+      output = output + sentence.charAt(i);
+    }
+    i++;
+  }
+  document.getElementById("output").innerHTML = "Your sentence in Hacker Speak is: <span class='answer'>" + output + "</span>";
+}
